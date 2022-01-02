29550380b21630784437d29e9358e5ce5aadf619
Olusola Afikode <afikodeolaitan@gmail.com>
2022-01-03 00:29:57 +0100

    add button to delete each person on list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Flex, Text } from "@chakra-ui/react";
+import "./App.css";
+import Event from "./Event";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      minHeight="90vh"
+      margin="0 auto"
+      maxWidth="1170px"
+      width="90vw"
+      textAlign="center"
+      marginTop="5rem"
+      mb="5rem"
+      pt="5rem"
+      pb="1.5rem"
+      pl="2rem"
+      pr="2rem"
+      borderRadius="0.25rem"
+      boxShadow="0 5px 15px rgba(0, 0, 0, 0.4)"
+      bg="whiteAlpha.400"
+    > 
+      <Event />
+    </Box>
   );
 }
 
---
+++ src/Event.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { data } from "./data";
+
+import { Box, Flex, Text, Image, Button, Grid } from "@chakra-ui/react";
+
+const Event = () => {
+  const [people, setPeople] = useState(data);
+
+  const removerPerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+
+  return (
+    <Box>
+      <Text textAlign="left" pl="5" mb="5" fontWeight="bold">
+        {people.length} birthdays today
+      </Text>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <>
+            <Grid
+              templateColumns="auto 1fr"
+              alignItems="center"
+              columnGap="0.75rem"
+              mb="1.5rem"
+              key={id}
+            >
+              <Image
+                mb="5"
+                borderRadius="full"
+                boxSize="100px"
+                src={image}
+                alt="name"
+                objectFit="cover"
+              />
+              <Box pl="3">
+                <Text fontWeight="bold">{name}</Text>
+                <Text>{age} years</Text>
+                <Button mb="3" onClick={() => removerPerson(id)}>
+                  Delete Person
+                </Button>
+              </Box>
+            </Grid>
+          </>
+        );
+      })}
+
+      <Button onClick={() => setPeople([])}>Clear All</Button>
+    </Box>
+  );
+};
+
+export default Event;
